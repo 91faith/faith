a36a4c4ab13a8e85eb778f5ec8be18e37082066a
91faith <2060768145@qq.com>
2023-07-05 02:04:53 +0800

    .

--- .vuepress/theme/lib/client/composables/useSortSeries.d.ts
+++
@@ -1,5 +0,0 @@
-declare function sortSeries(arr: any): any;
-export declare function useSortSeries(): {
-    sortSeries: typeof sortSeries;
-};
-export {};
--- docs/JYSC/JY.md
+++
@@ -1,6 +0,0 @@
----
-title: sdfsd
-date: 2020/05/29
----
-
-This is api.
